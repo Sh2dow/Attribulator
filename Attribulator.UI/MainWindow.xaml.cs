9a81fa8926b26252e42b2f38500db4925a570665
ARCHIE <ARCHIE@ARCHIE-PC>
2024-02-02 16:43:33 +0100

    Added UI

---
+++ Attribulator.UI/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+using Forms = System.Windows.Forms;
+
+namespace AttribulatorUI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private string gameFolder;
+
+		private IServiceProvider serviceProvider;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MenuItem_Open_Click(object sender, RoutedEventArgs e)
+		{
+			using (var dialog = new Forms.FolderBrowserDialog())
+			{
+				Forms.DialogResult result = dialog.ShowDialog();
+
+				if (result == Forms.DialogResult.OK)
+				{
+					this.gameFolder = dialog.SelectedPath;
+
+
+				}
+			}
+		}
+	}
+}
