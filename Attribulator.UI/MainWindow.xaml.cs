34e79901b55cb9693fc0ff076d9ba7c64a065720
ARCHIE <ARCHIE@ARCHIE-PC>
2024-02-02 17:44:55 +0100

    Unpack works

--- Attribulator.UI/MainWindow.xaml.cs
+++ Attribulator.UI/MainWindow.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Attribulator.API.Services;
+using Attribulator.CLI;
+using Attribulator.CLI.Services;
+using Attribulator.Plugins.SpeedProfiles;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
 using System.Windows;
-
+using VaultLib.Core.DB;
 using Forms = System.Windows.Forms;
 
 namespace AttribulatorUI
@@ -17,6 +23,30 @@ namespace AttribulatorUI
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			// Setup
+
+			var services = new ServiceCollection();
+			var loaders = Program.GetPluginLoaders();
+
+			// Register services
+			services.AddSingleton<ICommandService, CommandServiceImpl>();
+			services.AddSingleton<IProfileService, ProfileServiceImpl>();
+			services.AddSingleton<IStorageFormatService, StorageFormatServiceImpl>();
+			services.AddSingleton<IPluginService, PluginServiceImpl>();
+
+			// Set up logging
+			Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.Console().CreateLogger();
+			services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+
+			var plugins = Program.ConfigurePlugins(services, loaders);
+			serviceProvider = services.BuildServiceProvider();
+
+			// Load everything from DI container
+			Program.LoadCommands(services, serviceProvider);
+			Program.LoadProfiles(services, serviceProvider);
+			Program.LoadStorageFormats(services, serviceProvider);
+			Program.LoadPlugins(plugins, serviceProvider);
 		}
 
 		private void MenuItem_Open_Click(object sender, RoutedEventArgs e)
@@ -28,8 +58,18 @@ namespace AttribulatorUI
 				if (result == Forms.DialogResult.OK)
 				{
 					this.gameFolder = dialog.SelectedPath;
+					D
+					var profile = new CarbonProfile();
+					var database = new Database(new DatabaseOptions(profile.GetGameId(), profile.GetDatabaseType()));
+					var files = profile.LoadFiles(database, this.gameFolder + "\\GLOBAL");
+					database.CompleteLoad();
 
+					var storageFormat = serviceProvider.GetRequiredService<IStorageFormatService>().GetStorageFormat("yml");
+					storageFormat.Serialize(database, this.gameFolder + "\\GLOBAL123", files);
 
+					int a;
+					a = 1;
+					a++;
 				}
 			}
 		}
